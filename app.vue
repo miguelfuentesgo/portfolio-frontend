347f3b738c9d34e06998651605c2905ef1e6598a
Miguel Fuentes <miguel_eduard98@hotmail.com>
2025-05-07 15:06:57 -0500

    initial commit portfolio

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>
+      Hola mundo Portfolio!
+    </h1>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  ssr: false,
+  css: [
+    '@/assets/scss/main.scss'
+
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/assets/scss/variables" as *;`  
+        }
+      }
+    }
+  }
+})
